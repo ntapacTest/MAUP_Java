444a8f443c9da857f982b0ce3d2c37508697b29e
vshu <you@example.com>
2019-05-20 21:05:05 +0300

    Work with strings

---
+++ src/les_03_strings/Task01.java
@@ -0,0 +1,39 @@
+package les_03_strings;
+
+import java.util.Scanner;
+
+public class Task01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input PIB:");
+        String fio=scanner.nextLine();
+
+        //String fio="qqqqqq wwwww eeeee";
+
+        Boolean pr=fio.contains(" ");
+        int lastIndex=0;
+
+        while (pr) {
+            if(pr)
+            {
+                lastIndex=fio.indexOf(" ");
+                System.out.println(fio.substring(0,lastIndex));
+                fio=fio.substring(lastIndex+1);
+
+                pr=fio.contains(" ");
+            }
+        }
+        System.out.println(fio);
+
+        /*
+        String[] fioArray;
+
+        fioArray=fio.split(" ");
+
+        for (int i = 0; i < fioArray.length; i++) {
+            System.out.println(fioArray[i]);
+        }
+        */
+    }
+}
--- src/les_03_strings/TestStringsEquals.java
+++ src/les_03_strings/TestStringsEquals.java
@@ -2,11 +2,11 @@ package les_03_strings;
 
 public class TestStringsEquals {
     public static void main(String[] args) {
-        String s1=new String("java");
-        String s2=new String("java");
+        String s1=new String("java"); // first memory address
+        String s2=new String("java"); // second memory address
 
-        String s3="java";
-        String s4="java";
+        String s3="java"; // third memory address
+        String s4="java"; // third memory address
 
         System.out.println(s1==s2);
         System.out.println(s1.equals(s2));
